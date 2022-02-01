ef97b65faf51e308d759e164c5bf42bb3843b4ec
joannakolawole1 <98406259+joannakolawole1@users.noreply.github.com>
2022-02-01 16:45:16 +0100

    Add files via upload

---
+++ Feb 1.R
@@ -0,0 +1,17 @@
+# Author: Joanna, Date: Feb 1, Purpose: Test descriptive variables
+
+#Calculate the standard deviation  for dummy data
+
+dummy_data <- c(1,5,6,3,4,2)
+
+sd(dummy_data)
+
+
+#calculate the median for dummy data
+
+median(dummy_data)
+
+
+#ca
+
+
